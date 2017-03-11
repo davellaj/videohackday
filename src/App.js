64bf2dc3d55d7d39ea885c38ad4843769222b4ef
davellaj <jamie.davella@gmail.com>
2017-03-11 11:56:51 -0500

    initialize components :

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import ProductImg from './components/productImg'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Video hackday</h2>
+      <div className="Container">
+        <div className="App-body">
+          <h2 className="title">DMS</h2>
         </div>
         <p className="App-intro">
           put video here
         </p>
+        <ProductImg />
       </div>
     );
   }
---
+++ src/components/productImg.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProductImg = (props) => {
+  return(
+    <div>
+       This is the product video
+    </div>
+  )
+}
+ export default ProductImg
