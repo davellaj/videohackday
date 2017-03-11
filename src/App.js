d7f85948140bec6fa1624647fa3c5f0c7a83e18d
davellaj <jamie.davella@gmail.com>
2017-03-11 11:16:14 -0500

    initialize create react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Video hackday</h2>
+        </div>
+        <p className="App-intro">
+          put video here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
